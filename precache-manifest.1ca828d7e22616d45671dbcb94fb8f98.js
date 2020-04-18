59936235db9b460e5611b042b04fda9dc3c04f7a
Alan Sax <alan@lelander.com>
2020-04-18 00:56:21 -0500

    Updates

---
+++ precache-manifest.1ca828d7e22616d45671dbcb94fb8f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b3690c6a6961d9d23866e9626c9901",
+    "url": "/saxa-react/index.html"
+  },
+  {
+    "revision": "733f93ab1fac3f91f90d",
+    "url": "/saxa-react/static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "4cbf08a60e79bec5607b",
+    "url": "/saxa-react/static/js/2.57c752a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saxa-react/static/js/2.57c752a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733f93ab1fac3f91f90d",
+    "url": "/saxa-react/static/js/main.adae5889.chunk.js"
+  },
+  {
+    "revision": "943a87d5b5c61f70b0e4",
+    "url": "/saxa-react/static/js/runtime-main.4616d35f.js"
+  }
+]);
